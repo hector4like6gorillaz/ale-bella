cd23940832da121f7a1ab929819abda910f49b21
HectorBalan <balan.huchin.3@gmail.com>
2024-11-26 17:43:29 -0600

    add changes in services

--- src/components/bannerTop/BannerTop.tsx
+++ src/components/bannerTop/BannerTop.tsx
@@ -13,11 +13,19 @@ const BannerTop = ({
       </div>
       <div className={`${style['background-black-container']}`}>
         <div className={`${style['div-titles-container']}`}>
+          {/*
           <p className={`${style['p-title']}`}>What We Do​</p>
           <br />
           <h3 className={`${style['h3-title']}`}>A tailored approach</h3>
           <h3 className={`${style['h3-title']}`}>to connect Brands</h3>
           <h3 className={`${style['h3-title']}`}>and Customers</h3>
+          */}
+          <p className={`${style['p-title']}`}>
+            A powerful brand is built on more than just a logo and slogan.
+          </p>
+          <p className={`${style['p-title']}`}>
+            We craft brands that captivate attention and ignite curiosity.
+          </p>
         </div>
       </div>
       <Wave className={`${style['wave-class']}`} />
