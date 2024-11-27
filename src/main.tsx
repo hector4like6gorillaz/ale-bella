a2eb41bcb2f930219cd8ab580cdc682be26c1baf
HectorBalan <balan.huchin.3@gmail.com>
2024-11-26 19:56:31 -0600

    add changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store/index.ts'
 import Router from './router/Router.tsx'
-import './styles/global.scss'
 import '@coreui/coreui/dist/css/coreui.min.css'
-
+import './styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
